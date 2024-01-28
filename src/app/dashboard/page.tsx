e7252a19a16b744cdf08b1cb12d837c3fc5534ee
soumya100 <47693557+soumya100@users.noreply.github.com>
2024-01-28 20:18:41 +0530

    layout set

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/common'
+import { FC } from 'react'
+
+interface pageProps {
+  
+}
+
+const page: FC<pageProps> = ({}) => {
+  return <div>
+    <Button> Hello </Button>
+  </div>
+}
+
+export default page
