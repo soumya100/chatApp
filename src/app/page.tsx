4867b5d267dbcf93e05782f3387352a5a646c935
soumya100 <47693557+soumya100@users.noreply.github.com>
2024-01-28 21:22:22 +0530

    db connected

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import { db } from "@/lib/db";
 import Image from "next/image";
 
-export default function Home() {
+
+export default async function Home() {
+
+  await db.set("hello", "hello")
+
   return (
     <main>
-    
+    hello world
     </main>
   );
 }
